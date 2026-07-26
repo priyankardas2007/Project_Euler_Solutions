77a0b2e9eba9669e727899cb8ff300fcee4dbe64
Priyankar Das <daspriyankar2005@gmail.com>
2026-07-26 09:09:08 +0530

    Implement function to sum multiples of 3 or 5

---
+++ Problem-001/solution.js
@@ -0,0 +1,11 @@
+function multiplesOf3Or5(number) {
+  let total = 0;
+
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      total += i;
+    }
+  }
+
+  return total;
+}
